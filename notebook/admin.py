b8841a32d1defcca084bb83fbd8cb4cc36a14663
Tyson Manwarren <tmanwarren@empirenatgas.com>
2021-08-12 15:14:40 -0400

    added some admin features

--- notebook/admin.py
+++ notebook/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Tags
+from .models import Tags, Notebook, NotebookTab
 
 
 @admin.register(Tags)
 class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Notebook)
+class NotebookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(NotebookTab)
+class NotebookTabAdmin(admin.ModelAdmin):
     pass
