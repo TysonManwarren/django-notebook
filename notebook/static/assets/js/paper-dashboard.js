53afeb250627561032fa7e110d82b439d7baa82e
Tyson Manwarren <tmanwarren@empirenatgas.com>
2021-08-18 07:58:01 -0400

    custom checklist plugin

--- notebook/static/assets/js/paper-dashboard.js
+++ notebook/static/assets/js/paper-dashboard.js
@@ -203,16 +203,16 @@ function hexToRGB(hex, alpha) {
 tinymce.init({
   selector: 'textarea',  // change this value according to your HTML
   images_upload_url: '/upload_image/', // Image upload address in Django route
-  height: 500,
+  height: 440,
   plugins: [
       'advlist autolink lists link image charmap print preview anchor',
       'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table paste code help wordcount'
+      'insertdatetime media table paste code help wordcount mychecklist'
   ],
   menubar: 'favs file edit view insert format tools table help',
   toolbar: 'undo redo | formatselect | ' +
       'bold italic backcolor | alignleft aligncenter ' +
-      'alignright alignjustify | bullist numlist outdent indent | ' +
+      'alignright alignjustify | bullist numlist mychecklist outdent indent | ' +
       'code image | ' +
       'removeformat | help',
 });
